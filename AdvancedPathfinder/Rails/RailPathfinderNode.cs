3d2631b44d7f022c126cd27fc31c88dccab55e75
Michal Novotny <xmnovotny@gmail.com>
2021-08-16 18:30:36 +0200

    Removed generic PathfinderNode

--- AdvancedPathfinder/Rails/RailPathfinderNode.cs
+++ AdvancedPathfinder/Rails/RailPathfinderNode.cs
@@ -1,22 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using VoxelTycoon;
 using VoxelTycoon.Tracks;
 using VoxelTycoon.Tracks.Rails;
 
 namespace AdvancedPathfinder.Rails
 {
-    public class RailPathfinderNode: PathfinderNode<Rail, RailConnection, RailSection, RailPathfinderEdge>
+    public class RailPathfinderNode: PathfinderNodeBase
     {
         public bool IsElReachable { get; private set; }
         public int NumPassableOutboundEdges { get; private set; }  //number of passable edges, that leads to this node
         private Dictionary<PathfinderNodeBase, float> _elReachableNodes;
         private readonly Dictionary<(int destinationHash, bool electric), bool> _pathDiversionCache = new();
+        private readonly HashSet<RailConnection> _inboundConnections = new();
+        private readonly HashSet<RailConnection> _outboundConnections = new();
+        private readonly List<RailPathfinderEdge> _edges = new();
 
-        public override Dictionary<PathfinderNodeBase, float> GetReachableNodes(object edgeSettings)
+        private Dictionary<PathfinderNodeBase, float> _reachableNodes;
+        
+        private bool _initialized = false;
+
+        public IReadOnlyCollection<RailConnection> InboundConnections => _inboundConnections;
+        public IReadOnlyCollection<RailConnection> OutboundConnections => _outboundConnections;
+        public ImmutableList<RailPathfinderEdge> Edges => _edges.ToImmutableList();
+        [CanBeNull] 
+        internal new RailPathfinderNode PreviousBestNode => (RailPathfinderNode) base.PreviousBestNode;
+        [CanBeNull] 
+        internal new RailPathfinderEdge PreviousBestEdge => (RailPathfinderEdge) base.PreviousBestEdge;
+
+        internal override IReadOnlyList<PathfinderEdgeBase> GetEdges()
+        {
+            return _edges;
+        }
+        
+        public Dictionary<PathfinderNodeBase, float> GetReachableNodes(object edgeSettings)
         {
             if (edgeSettings is not RailEdgeSettings {Electric: true})
             {
-                return base.GetReachableNodes(edgeSettings);
+                return _reachableNodes;
             }
 
             return _elReachableNodes;
@@ -64,17 +86,64 @@ namespace AdvancedPathfinder.Rails
             return false;
         }
 
-        internal override void SetReachableNodes(Dictionary<PathfinderNodeBase, float> reachableNodes, object edgeSettings)
+        internal void Initialize(RailConnection inboundConnection, bool trackStart = false)
+        {
+            if (_initialized)
+                throw new InvalidOperationException("Already initialized");
+            _initialized = true;
+            if (trackStart)
+            {
+                if (inboundConnection.OuterConnectionCount > 0)
+                {
+                    throw new InvalidOperationException("Some outer connection on start connection");
+                }
+
+                _outboundConnections.Add(inboundConnection);  //add provided connection os outbound (there will be no inbound connections)
+            }
+            else
+            {
+                foreach (TrackConnection conn in inboundConnection.OuterConnections)
+                {
+                    _outboundConnections.Add((RailConnection) conn);
+                }
+
+                if (inboundConnection.OuterConnectionCount > 0)
+                {
+                    TrackConnection outConn = inboundConnection.OuterConnections[0];
+                    foreach (TrackConnection conn in outConn.OuterConnections)
+                    {
+                        _inboundConnections.Add((RailConnection) conn);
+                    }
+                }
+                else
+                {
+                    //end connection, add provided connection as only inbound
+                    _inboundConnections.Add(inboundConnection);
+                }
+            }
+        }
+        
+        internal void FindEdges(ISectionFinder sectionFinder, INodeFinder nodeFinder)
+        {
+            foreach (RailConnection connection in _outboundConnections)
+            {
+                RailPathfinderEdge edge = new() {Owner = this};
+                if (!edge.Fill(connection, sectionFinder, nodeFinder))
+                    continue;
+                _edges.Add(edge);
+                ProcessNewEdge(edge);
+            }
+        }
+        
+        internal void SetReachableNodes(Dictionary<PathfinderNodeBase, float> reachableNodes, object edgeSettings)
         {
             if (edgeSettings is not RailEdgeSettings {Electric: true})
-            {
-                base.SetReachableNodes(reachableNodes, edgeSettings);
-            }
-
-            _elReachableNodes = reachableNodes;
+                _reachableNodes = reachableNodes;
+            else
+                _elReachableNodes = reachableNodes;
         }
 
-        protected override void ProcessNewEdge(RailPathfinderEdge edge)
+        private void ProcessNewEdge(RailPathfinderEdge edge)
         {
             if (edge.IsPassable())
             {
